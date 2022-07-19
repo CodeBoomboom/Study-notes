33ebd709021c80fec12fcaebb8fd91c52e745d7c
CodeBoomboom <xiaodexin0701@163.com>
2022-07-19 22:32:32 +0800

    add

--- DataStruct/Test/Test/计算PWM.cpp
+++ DataStruct/Test/Test/计算PWM.cpp
@@ -1,16 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void test01() {
-	
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+vector<int> vec;
+void travserval(TreeNode* root) {
+    if (root == NULL)    return;
+    travserval(root->left);
+    vec.push_back(root->val);
+    travserval(root->right);
+}
+vector<int> findMode(TreeNode* root) {
+    travserval(root);
+    vector<int> result;
+    int pre = INT_MAX;
+    for (vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+        if (*it == pre) {
+            if (result.empty()) {
+                result.push_back(pre);
+                result.push_back(*it);
+            }
+            else {
+                if (result.back() == *it) {
+                    result.push_back(*it);
+                }
+                else {
+                    result.push_back(pre);
+                    result.push_back(*it);
+                }
+            }
+
+        }
+        pre = *it;
+    }
+    return result;
 }
 
 int main(){
-    string s;
-    while (cin >> s) {
-        if (cin.get() == '\n') break;
-    }
-    cout << s.size();
-    return 0;
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(2);
+    vector<int> ret;
+
+    ret = findMode(root);
 }
