98df37c65ea362bd1d1853243e9f88dbeea47c03
CodeBoomboom <xiaodexin0701@163.com>
2022-07-25 22:39:07 +0800

    1

--- DataStruct/Test/Test/计算PWM.cpp
+++ DataStruct/Test/Test/计算PWM.cpp
@@ -1,54 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
-#include<vector>
+#include<string>
+#include<map>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-};
-vector<int> vec;
-void travserval(TreeNode* root) {
-    if (root == NULL)    return;
-    travserval(root->left);
-    vec.push_back(root->val);
-    travserval(root->right);
-}
-vector<int> findMode(TreeNode* root) {
-    travserval(root);
-    vector<int> result;
-    int pre = INT_MAX;
-    for (vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
-        if (*it == pre) {
-            if (result.empty()) {
-                result.push_back(pre);
-                result.push_back(*it);
-            }
-            else {
-                if (result.back() == *it) {
-                    result.push_back(*it);
-                }
-                else {
-                    result.push_back(pre);
-                    result.push_back(*it);
-                }
-            }
-
-        }
-        pre = *it;
+int main() {
+    string str = {};
+    char ch = {};
+    getline(cin, str);
+    cin >> ch;
+    map<char, size_t> mul;
+    for (auto i : str) {
+        char temp = tolower(i);
+        mul[temp] +=1;
     }
-    return result;
-}
+    cout << mul[tolower(ch)] << endl;
 
-int main(){
-    TreeNode* root = new TreeNode(1);
-    root->right = new TreeNode(2);
-    root->right->left = new TreeNode(2);
-    vector<int> ret;
-
-    ret = findMode(root);
 }
